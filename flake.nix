b41d9223bcf042f9f4029e08b361c69c1d7df574
Yuri Albuquerque <yuridenommus@gmail.com>
2024-12-29 08:23:09 -0300

    Overriding dune_3

--- flake.nix
+++ flake.nix
@@ -16,9 +16,27 @@
       system:
       let
         pname = "netwire";
+        dune_src =
+          prev:
+          prev.fetchFromGitHub {
+            owner = "ocaml";
+            repo = "dune";
+            rev = "34de4d1264240f2bddeda4128a2bd285488341ab";
+            hash = "sha256-kO9nwa0RrOZvpLvISuqyRDl+g1sJ7IGnpbF0el+MCn8=";
+          };
         overlay = (
           final: prev: {
             "${pname}" = final.ocamlPackages.callPackage ./. { inherit pname; };
+            dune_3 = prev.dune_3.overrideAttrs (old: {
+              src = dune_src prev;
+            });
+            ocamlPackages = prev.ocamlPackages.overrideScope (
+              ofinal: oprev: {
+                dune_3 = oprev.dune_3.overrideAttrs (old: {
+                  src = dune_src prev;
+                });
+              }
+            );
           }
         );
         pkgs = import nixpkgs {
